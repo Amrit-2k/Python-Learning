0632a1cfa4f2cf69cd1e5a5ca3c17810b553565c
Amrit-2k <amritbir.m@gmail.com>
2023-02-21 23:47:30 +1300

    Attribute error fixed

--- auto-job-apply/main.py
+++ auto-job-apply/main.py
@@ -10,42 +10,41 @@ options = Options()
 options.add_argument("start-maximized")
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-
+#your email and password here
 email =""
 pass_word = ""
 
-driver.get("https://www.linkedin.com/home")
+#main py
+try:
+    driver.get("https://www.linkedin.com/home")
 
-user_name = driver.find_element(By.ID, "session_key")
-user_name.send_keys(email)
+    try:
+       
+        user_name = driver.find_element(By.ID, "session_key")
+        user_name.send_keys(email)
 
-password = driver.find_element(By.ID, "session_password")
-password.send_keys(pass_word)
+        password = driver.find_element(By.ID, "session_password")
+        password.send_keys(pass_word)
 
+        sign_in = driver.find_element(By.CLASS_NAME, "sign-in-form__submit-button")
+        sign_in.click()
+        time.sleep(5)
 
-sign_in = driver.find_element(By.CLASS_NAME, "sign-in-form__submit-button")
-sign_in.click()
+    except AttributeError:
+        print("Already signed in")
 
+    try:  
 
-time.sleep(5)
-
-driver.get("https://www.linkedin.com/jobs/search/?currentJobId=3451156668&f_AL=true&geoId=105490917&keywords=electronic%20engineer&location=New%20Zealand&refresh=true")
-
-time.sleep(5)
-submit = driver.find_element(By.CLASS_NAME, "jobs-save-button")
-submit.click()
-
-
-
-time.sleep(5)
-
-
-
-
-
-
+        driver.get("https://www.linkedin.com/jobs/search/?currentJobId=3451156668&f_AL=true&geoId=105490917&keywords=electronic%20engineer&location=New%20Zealand&refresh=true")
+        time.sleep(5)
+        submit = driver.find_element(By.CLASS_NAME, "jobs-save-button")
+        submit.click()
     
+    except AttributeError:
+        print("Already applied for this job")
 
+except AttributeError:
+    print("No more jobs to apply for")
 
-
+time.sleep(5)
 driver.close()
